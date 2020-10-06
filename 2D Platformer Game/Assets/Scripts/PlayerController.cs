37a79dfc94d8729fb2b961c1d311bf738e029290
Filip Đukić <38077472+filipdjukic96@users.noreply.github.com>
2020-10-06 23:23:37 +0200

    Player added
    - Player added
    - Sprites sorted with different layers
    - Physics added to player (Rigidbody 2D and Capsule Collider 2D)
    - PlayerController script added
    - PlayerController script modified for moving and jumping

---
+++ 2D Platformer Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    public float jumpForce;
+    public Rigidbody2D rigidBody;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rigidBody.velocity = new Vector2(moveSpeed * Input.GetAxis("Horizontal"), rigidBody.velocity.y);
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpForce);
+        }
+    }
+}

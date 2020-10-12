df239e952afc80082a14f84286e0a7b507182231
Filip Đukić <38077472+filipdjukic96@users.noreply.github.com>
2020-10-12 22:46:05 +0200

    Player improvements
    - Rigidbody 2D Collision Detection changed from Discrete to Continuous (so player's feet don't fall through the ground)
    - Physics material added to player (friction 0 - player doesn't get stuck when jumping near an edge)
    - Unlimited jumping stopped (Ground Point added to player's feet)
    - Double jump option added
    - Player animations added (idle, running, jumping)
    - Animations transitions added (controlled through code)

--- 2D Platformer Game/Assets/Scripts/PlayerController.cs
+++ 2D Platformer Game/Assets/Scripts/PlayerController.cs
@@ -4,25 +4,72 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed;
+    //PUBLIC//
+    public float moveSpeed; 
     public float jumpForce;
-    public Rigidbody2D rigidBody;
+    public Rigidbody2D rigidBody; //player's rigid body component - set through editor
 
+
+    public Transform groundCheckPoint; //Transform object of Ground Point at the player's feet
+    public LayerMask whatIsGround; //check what layer is Ground
+
+
+    //PRIVATE//
+    private bool isGrounded;//denotes if the player is touching the ground
+    private bool canDoubleJump; //denotes if the player is allowed a second jump
+
+    private Animator animator;//Animator component attached to the player
+    private SpriteRenderer spriteRenderer; //SpriteRenderer component attached to the player
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //find the component of type Animator that is attached to the same object this script is attached to
+        //script automatically recognizes
+        animator = GetComponent<Animator>();
+        //find the component of type SpriteRenderer that is attached to the same object this script is attached to
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
         rigidBody.velocity = new Vector2(moveSpeed * Input.GetAxis("Horizontal"), rigidBody.velocity.y);
+       
+        //does an overlap and checks if any other object collides with the circle
+        //creates a circle at groundCheckPoint.position of diameter .2f and checks if it collides with any object
+        //on Ground layer (whatIsGround)
+        isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position, .2f, whatIsGround);
 
-        if(Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump"))
         {
-            rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpForce);
+            if(isGrounded) //only if grounded
+            {
+                rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpForce);
+                canDoubleJump = true;
+            }
+            else
+            {
+                if (canDoubleJump)
+                {
+                    rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpForce);
+                    canDoubleJump = false;
+                }
+            }
+            
         }
+
+
+        //set the Animator's parameters
+        animator.SetBool("isGrounded", isGrounded);
+        animator.SetFloat("moveSpeed", Mathf.Abs(rigidBody.velocity.x));
+
+        //change direction of the player depending on if he moves forward/backwards
+        //ONLY if x component of velocity is !=0 (so the player stays facing the desired direction when stopped)
+        if (rigidBody.velocity.x != 0)
+        {
+            spriteRenderer.flipX = (rigidBody.velocity.x < 0);
+        }
+        
     }
 }

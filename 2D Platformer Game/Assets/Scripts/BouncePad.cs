e8ff2d5aaebc69d666aeb9291b41da0bfed231e1
Filip Đukić <38077472+filipdjukic96@users.noreply.github.com>
2020-11-12 18:51:16 +0100

    More level mechanics pt. 2
    - Bounce pad added
    - Switch added (activate/deactivate an object)

---
+++ 2D Platformer Game/Assets/Scripts/BouncePad.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BouncePad : MonoBehaviour
+{
+    // PUBLIC //
+
+    public float bounceForce = 20f; //force to fling the Player
+
+    // PRIVATE //
+
+    private Animator animator; //animator of the Bouncer
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            //change the Player velocity Y component to bounceForce
+            PlayerController.instance.rigidBody.velocity = new Vector2(PlayerController.instance.rigidBody.velocity.x, bounceForce);
+
+            //set trigger for animation
+            animator.SetTrigger("Bounce");
+        }
+    }
+}
---
+++ 2D Platformer Game/Assets/Scripts/Switch.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour
+{
+    // PUBLIC //
+    public GameObject objectToSwitch;
+
+    //sprite to switch to when the SWITCH is activated
+    public Sprite switchDownSprite;
+
+    //denotes if the switch deactivates or activates upon trigger
+    public bool deactivatesOnSwitch;
+
+
+
+    // PRIVATE //
+    
+    private SpriteRenderer spriteRenderer;
+
+    //was the switch used
+    private bool hasSwitched;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player") && !hasSwitched)
+        {
+            //mark switched
+            hasSwitched = true;
+
+            //deactivate the door
+            objectToSwitch.SetActive(deactivatesOnSwitch ? false : true);
+
+            //change the switch sprite to switch-down
+            spriteRenderer.sprite = switchDownSprite;
+        }
+    }
+}

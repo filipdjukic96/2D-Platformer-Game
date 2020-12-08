3ef17b991b546a5ff688bfbdac63cd89291b5623
Filip Đukić <38077472+filipdjukic96@users.noreply.github.com>
2020-12-08 21:14:55 +0100

    Levels two & three created
    - Level_2 & Level_3

--- 2D Platformer Game/Assets/Scripts/Boss/Monster Boss/BossMonsterController.cs
+++ 2D Platformer Game/Assets/Scripts/Boss/Monster Boss/BossMonsterController.cs
@@ -43,7 +43,7 @@ public class BossMonsterController : MonoBehaviour
 
         var currentHealth = bossHealthController.CurrentHealth();
 
-        if (currentHealth <= 4)
+        if (currentHealth <= 2)
         {
             animator.SetTrigger("stageTwo");
         }
